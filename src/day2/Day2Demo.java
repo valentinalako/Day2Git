397bf37c32355dc4f96222493857e87c64cd2e42
valen <valen@DESKTOP-057E61O.lan>
2023-04-07 20:31:09 -0400

    added note

--- src/day2/Day2Demo.java
+++ src/day2/Day2Demo.java
@@ -2,9 +2,8 @@ package day2;
 
 public class Day2Demo {
 	
-	public static void main (String arg []) {
+	public void getInfo() {
+		System.out.println("Pushing code...");
 		
-		System.out.println("Day 2 GIT");
 	}
-
 }
